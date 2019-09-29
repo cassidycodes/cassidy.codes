c4960cddb2a3a5b90389b4061b61ea70272cfade
Cassidy K <hello@cassidy.codes>
2019-09-29 19:23:57 -0400

    Persisting mode across requests

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,12 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
     return (context || document).querySelectorAll(selector);
   }
 
-  // And select the elements we're going to be working with.
-  var $toggle = $('.mask')[0];
-
-  // The slector returns a NodeList, but we only care about the first one.
-  $toggle.addEventListener('click', function() {
+  // Toggle dark/light mode.
+  function toggleMode() {
     $('.icon-wrap')[0].classList.toggle('active');
     $('body')[0].classList.toggle('dark');
+  };
+
+  // Store this setting for the next request!
+  function saveSetting() {
+    // localStorage only saves strings. Booleans will get stringified here.
+    var darkmode = localStorage.getItem('mode');
+    localStorage.setItem('mode', (darkmode == 'dark' ? 'light' : 'dark'));
+  };
+
+  localStorage.getItem('mode') == 'dark' ? toggleMode() : '';
+
+  // And select the elements we're going to be working with.
+  // The slector returns a NodeList, but we only care about the first one.
+  $('.mask')[0].addEventListener('click', function() {
+    toggleMode();
+    saveSetting();
   });
 });
